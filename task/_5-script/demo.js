f04fabb9e51d540db86667075a114309a41cdea8
whiskey <2451925427@qq.com>
2017-10-09 14:05:19 +0800

    add ';'

--- task/_5-script/demo.js
+++ task/_5-script/demo.js
@@ -3,14 +3,14 @@ let fs = require('fs');
 let dirName = process.argv[2];
 if (fs.existsSync(dirName)){
     console.log("error: " + dirName + " exists");
-    process.exit(1)
+    process.exit(1);
 }else{
     fs.mkdirSync(dirName);
     process.chdir(dirName);
-    fs.mkdirSync('css')
-    fs.mkdirSync('js')
-    fs.writeFileSync('index.html','<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>')
-    fs.writeFileSync('css/style.css','h1{color: red;}')
-    fs.writeFileSync('js/main.js','var string = "Hello World"\nalert(string)')
-    process.exit(0)
+    fs.mkdirSync('css');
+    fs.mkdirSync('js');
+    fs.writeFileSync('index.html','<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>');
+    fs.writeFileSync('css/style.css','h1{color: red;}');
+    fs.writeFileSync('js/main.js','var string = "Hello World"\nalert(string)');
+    process.exit(0);
 }
